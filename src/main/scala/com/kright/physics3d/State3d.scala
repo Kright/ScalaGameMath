a59fda4dc15a8556e1e675cad05f9254399c432b
Igor Slobodskov <igor.slobodskov@phystech.edu>
2022-10-30 08:57:54 +0100

    add constructor to State3d

--- src/main/scala/com/kright/physics3d/State3d.scala
+++ src/main/scala/com/kright/physics3d/State3d.scala
@@ -2,6 +2,8 @@ package com.kright.physics3d
 
 class State3d(val transform: Transform3d,
               val velocity: Velocity3d):
+  def this() = this(Transform3d(), Velocity3d())
+  
   def updated(s: State3dDerivative, dt: Double): State3d =
     State3d(
       transform.copy().update(s.velocity, dt),
