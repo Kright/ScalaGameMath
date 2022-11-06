cb6b994e37207ed6cad215e0f61c60f19cbbd77c
Igor Slobodskov <igor.slobodskov@phystech.edu>
2022-11-06 17:53:37 +0100

    add State3d.copy()

--- src/main/scala/com/kright/physics3d/State3d.scala
+++ src/main/scala/com/kright/physics3d/State3d.scala
@@ -3,6 +3,9 @@ package com.kright.physics3d
 class State3d(val transform: Transform3d,
               val velocity: Velocity3d):
   def this() = this(Transform3d(), Velocity3d())
+  
+  def copy(): State3d =
+    State3d(transform.copy(), velocity.copy())
 
   def updated(s: State3dDerivative, dt: Double): State3d =
     State3d(
