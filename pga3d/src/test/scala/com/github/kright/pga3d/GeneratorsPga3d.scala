50468740f8eee5b9b63cab9b14b2899161953f30
Igor Slobodskov <igor.slobodskov@phystech.edu>
2025-02-15 22:34:29 +0100

    improve tests a bit

--- pga3d/src/test/scala/com/github/kright/pga3d/GeneratorsPga3d.scala
+++ pga3d/src/test/scala/com/github/kright/pga3d/GeneratorsPga3d.scala
@@ -4,10 +4,9 @@ import com.github.kright.math.VectorMathGenerators
 import org.scalacheck.Gen
 
 object GeneratorsPga3d:
-  private def makeGenT[T](elemsCount: Int, factory: (Array[Double], Int) => T): Gen[T] = {
+  private def makeGenT[T](elemsCount: Int, factory: (Array[Double], Int) => T): Gen[T] = 
     Gen.containerOfN[Array, Double](elemsCount, VectorMathGenerators.double1)
       .map(arr => factory(arr, 0))
-  }
 
   val bivectors: Gen[Pga3dBivector] =
     makeGenT(6, Pga3dSerializer.loadBivector)
@@ -23,3 +22,9 @@ object GeneratorsPga3d:
 
   val normalizedQuaternions: Gen[Pga3dQuaternion] =
     quaternions.filter(_.norm > 1e-40).map(_.normalizedByNorm)
+
+  val points: Gen[Pga3dPoint] =
+    makeGenT(3, Pga3dSerializer.loadPoint)
+
+  val vectors: Gen[Pga3dVector] =
+    makeGenT(3, Pga3dSerializer.loadVector)
