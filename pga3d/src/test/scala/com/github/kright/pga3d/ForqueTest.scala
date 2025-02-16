2e560b0e30bcab510c3f83364a219445db93fc91
Igor Slobodskov <igor.slobodskov@phystech.edu>
2025-02-16 14:12:21 +0100

    improve forque test

--- pga3d/src/test/scala/com/github/kright/pga3d/ForqueTest.scala
+++ pga3d/src/test/scala/com/github/kright/pga3d/ForqueTest.scala
@@ -3,14 +3,10 @@ package com.github.kright.pga3d
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class ForqueTest extends AnyFunSuiteLike:
-  test("force and momentum") {
-    val px: Double = 1.0
-    val py: Double = 2.0
-    val pz: Double = 3.0
+  test("force and torque") {
+    val torque = (Pga3dPoint(0, 1, 0) v Pga3dVector(1, 0, 0)) + (Pga3dPoint(0, -1, 0) v Pga3dVector(-1, 0, 0))
+    val force = (Pga3dPoint(0, 1, 0) v Pga3dVector(1, 0, 0)) + (Pga3dPoint(0, -1, 0) v Pga3dVector(1, 0, 0))
 
-    val f1 = Pga3dTrivector(0, 1, 0, 1) v Pga3dVector(1, 0, 0)
-    val f2 = Pga3dTrivector(0, -1, 0, 1) v Pga3dVector(-1, 0, 0)
-
-    assert((Pga3dVector(0, 0, 2) v Pga3dTrivector(0, 0, 0, 1)).dual === (f1 + f2))
-    assert((Pga3dTrivector(0, 0, 0, 1) v Pga3dVector(0, 0, 2)).dual * -1 === (f1 + f2))
+    assert(force === Pga3dBivector(0, 0, 0, 0, 0, 2))
+    assert(torque === Pga3dBivector(0, 0, -2, 0, 0, 0))
   }
